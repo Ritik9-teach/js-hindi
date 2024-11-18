a01192b9b1ac9609a3b8c432b845e0d875598e73
Ritik9-teach <ritikdogra69@gmail.com>
2024-11-18 18:16:07 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "ritikdogra69@gmail.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+let accountState;
+
+//accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "1122"
+accountCity = "jaipur"
+
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity, accountState]);
+
+/*
+prefer not user  var
+because of  issue  in block scope and  functional scope
+*/
